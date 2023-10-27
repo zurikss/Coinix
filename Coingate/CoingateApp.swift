63adf4be99bf8c4f903dd964e698242833fd92f9
Zurikss <victorander18@gmail.com>
2023-10-27 14:15:11 +0100

    Initial Commit

---
+++ Coingate/CoingateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoingateApp.swift
+//  Coingate
+//
+//  Created by Victor on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoingateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coingate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coingate
+//
+//  Created by Victor on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
